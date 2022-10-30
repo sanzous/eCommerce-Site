c40ab20cff15207a8c56f99d2a0656d10e87e458
sanzous <canastraleonard123@gmail.com>
2022-10-30 12:35:33 -0500

    Added banner component to App page, Passing sanity information to component

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
+import Link from 'next/link'
+import urlFor from '../lib/client'
 
-const Product = () => {
+const Product = ({ product }) => {
     return (
         <div>Product</div>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,39 @@
 import React from 'react'
-
+import { client } from '../lib/client'
 import { Product, FooterBanner, HeroBanner } from '../components'
+import banner from '../sanity_ecommerce/schemas/banner'
 
-const Home = () => {
+const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
-
+      <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+      {console.log(bannerData)}
       <div className='products-heading'>
         <h2>Best Selling Products</h2>
         <p>Speakers of many variations</p>
       </div>
 
       <div className='products-container'>
-        {['Product 1', 'Product 2'].map(
-          (product) => product
-        )}
+        {products?.map((product) => <Product key={product._id} prodcut={product} />)}
       </div>
 
       <FooterBanner />
     </>
   )
+}
+
+export const getServerSideProps = async () => {
+  //grabs all products from sanity dashboard
+  const query = '*[_type == "product"]'
+  const products = await client.fetch(query)
+
+  //grabs all banner products from sanity dashboard
+  const bannerQuery = '*[_type == "banner"]'
+  const bannerData = await client.fetch(bannerQuery)
+
+  return {
+    props: { products, bannerData }
+  }
 }
 
 export default Home
