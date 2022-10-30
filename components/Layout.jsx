a34ee4659a21f484371930b7c40a39476c613214
sanzous <canastraleonard123@gmail.com>
2022-10-30 15:49:37 -0500

    Layout component

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+//allows for use of metadata like a normal html head element
+import Head from 'next/head'
+import Navbar from './Navbar'
+import Footer from './Footer'
 
 const Layout = () => {
     return (
-        <div>Layout</div>
+        <div className='layout'>
+            <Head>
+                <title>Ecommerce Store</title>
+            </Head>
+            <header>
+                <Navbar />
+            </header>
+            <main className='main-container'>
+                Empty
+            </main>
+            <footer>
+                <Footer />
+            </footer>
+        </div>
     )
 }
 
