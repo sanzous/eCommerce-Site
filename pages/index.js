4785c1493732158f393a9dd3eab7c9ab7b969d62
sanzous <canastraleonard123@gmail.com>
2022-10-30 14:38:00 -0500

    Finished Footer Banner section

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ const Home = ({ products, bannerData }) => {
         {products?.map((product) => <Product key={product._id} product={product} />)}
       </div>
 
-      <FooterBanner FooterBanner={bannerData && bannerData[0]} />
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </>
   )
 }
