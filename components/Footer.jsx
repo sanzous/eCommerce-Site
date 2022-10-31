d5664ff1fceb05ef48dba7b5f74a074cb120ddde
sanzous <canastraleonard123@gmail.com>
2022-10-31 12:57:03 -0500

    Added navbar, deleted babelrc due to nextjs not reading babelrc correctly

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div>
+
+        </div>
     )
 }
 
