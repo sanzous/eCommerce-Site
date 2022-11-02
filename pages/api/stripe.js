f1065e666f4c55d2b81cba5a44a951edcb52a541
sanzous <canastraleonard123@gmail.com>
2022-11-02 17:02:35 -0500

    error tracking

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -40,6 +40,7 @@ export default async function handler(req, res) {
             const session = await stripe.checkout.sessions.create(params);
             res.status(200).json(session)
         } catch (err) {
+            console.log(err)
             res.status(err.statusCode || 500).json(err.message);
         }
     } else {
