79845c09ba6d7037cdb8d5cff413ecc6c014059e
sanzous <canastraleonard123@gmail.com>
2022-11-01 21:49:13 -0500

    Added StateContext, added dynamic cart functionality, added working inc and dec of items to add in product page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Layout } from '../components'
+import { Toaster } from 'react-hot-toast'
 import '../styles/globals.css'
 import { StateContext } from '../context/StateContext'
 
